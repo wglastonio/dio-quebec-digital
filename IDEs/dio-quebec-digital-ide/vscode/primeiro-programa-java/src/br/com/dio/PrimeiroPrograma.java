973b87638eddaac1cd4deef1e918616e9574db3f
wglastonio <wglastonio@gmail.com>
2022-10-12 15:43:12 -0300

    Training IDEs

---
+++ IDEs/dio-quebec-digital-ide/vscode/primeiro-programa-java/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        System.out.println("Teste inicial com IDE VS Code!");
+
+        Gato gato1 = new Gato("Azulzinho", "Azul", 5);
+        System.out.println(gato1);
+    }
+}
